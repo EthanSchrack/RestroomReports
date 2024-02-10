0533539b79e0485a0e29b23278189604e37934a2
Ethan Schrack <ethanschrack@gmail.com>
2024-02-09 22:48:55 -0500

    Implement ToiletList without objects

--- src/App.js
+++ src/App.js
@@ -2,32 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 import Hdr from './Hdr.js'
 import Bathroom from "./Bathroom.js";
+import ToiletList from './ToiletList.js';
 
 const bathroomPictureLink = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/VillaMitre50.jpg/1280px-VillaMitre50.jpg";
 
-const bathrooms = [
-  new Bathroom("Tate Bathroom", "abcd", 5.0, bathroomPictureLink),
-  new Bathroom("Joe Frank Bathroom", "abcd", 5.0, bathroomPictureLink),
-  new Bathroom("Snelling bathroom", "abcd", 5.0, bathroomPictureLink)
-]
 
-let bathroomElements = bathrooms.map(function(bathroom) {
-  return (
-    <div class="bathroom-element">
-      <span>
-        <img src={bathroom.image} width="75px" height="75px"/>
-        {bathroom.name}
-        ({bathroom.rating}/5)
-      </span>
-    </div>
-  )
-});
+// let bathroomElements = bathrooms.map(function(bathroom) {
+//   return (
+//     <div class="bathroom-element">
+//       <span>
+//         <img src={bathroom.image} width="75px" height="75px"/>
+//         {bathroom.name}
+//         ({bathroom.rating}/5)
+//       </span>
+//     </div>
+//   )
+// });
+
+const bathroomList = [
+  {
+    id: 'b1',
+    name: 'Terry toilet',
+    description: 'hellooo',
+    rating: '5.0',
+    image: bathroomPictureLink
+  },
+  {
+    id: 'b3',
+    name: 'Snelling toilet',
+    description: 'hellooo',
+    rating: '5.0',
+    image: bathroomPictureLink
+  }
+]
 
 function App() {
   return (
     <div>
       <Hdr />
-      {bathroomElements}
+      {/* {bathroomElements} */}
+      <ToiletList toilets={bathroomList} />
     </div>
   );
 }
--- src/Bathroom.js
+++ src/Bathroom.js
@@ -1,8 +1,21 @@
-export default class Bathroom {
-    constructor(name, description, rating, image) {
-        this.name = name;
-        this.description = description;
-        this.rating = rating;
-        this.image = image;
-    }
+// export default function Bathroom(n, d, r, i) {
+//         const name = n;
+//         const description = d;
+//         const rating = r;
+//         const image = i;
+
+const Bathroom = (props) => {
+        
+    return (
+        <div class="bathroom-element">
+            <span>
+                <h2>{props.name}</h2>
+                <p>{props.description} {props.rating} </p>
+
+                <img src={props.image} width="75px" height="75px"/>
+            </span>
+        </div>
+    );
 }
+
+export default Bathroom;
