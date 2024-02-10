3d17e4fb887b41444c26a5a801a57b5148da027a
whitnotmax <91440193+whitnotmax@users.noreply.github.com>
2024-02-10 14:16:07 -0500

    update & delete

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+var ObjectId = require('mongodb').ObjectId; 
 
 const app = express();
 app.use(express.json());
@@ -30,9 +32,6 @@ async function start() {
         console.log(`Example app listening on port ${PORT}`)
     });
 }
-    
-
-  
 
 
 app.get("/bathrooms", async (req, res) => {
@@ -48,19 +47,106 @@ app.get("/bathrooms", async (req, res) => {
 app.post("/add-bathroom", async (req, res) => {
     const collection = await db.collection("Bathrooms");
     
-    const document = req.body;
+    const body = req.body;
+
 
     ['name', 'description', 'image'].forEach(prop => {
-        if (!document[prop]) {
+        if (!body[prop]) {
             res.send(`Missing required property: ${prop}`).status(400);
+            return;
         }
-    })
+        
+    });
+
+    const document = {
+        id: uuidv4(),
+        name: body.name,
+        description: body.description,
+        image: body.image,
+        rating: 0.0
+    }
 
     const result = await collection.insertOne(document);
-    result.insertedId ? res.send({bathroom_id: result.insertedId}).status(200) : res.status(500).send();
+    result.insertedId ? res.send({bathroom_id: document.id}).status(200) : res.status(500).send();
 
 
 });
 
+// TODO GET REQUEST!
+app.post("/details", async (req, res) => {
+    const collection = await db.collection("Bathrooms");
+    const body = req.body;
+    console.log(body);
+    const id = body["id"];
+
+    if (!id) {
+        res.send("Missing bathroom ID").status(400);
+        return;
+    }
+
+    const result = await collection.findOne({"id": id});
+    res.send(result);   
+
+});
+
+app.put("/update-bathroom", async (req, res) => {
+    const collection = await db.collection("Bathrooms");
+    const body = req.body;
+
+    const id = body["id"];
+
+    if (!id) {
+        res.send("Missing bathroom ID").status(400);
+        return;
+    }
+    const updates = [];
+
+    ['name', 'description', 'image', 'rating'].forEach(prop => {
+        if (body[prop]) {
+            updates.push({[prop]: body[prop]});
+        }
+    });
+
+    let merged = {}
+    updates.forEach(update => {
+        Object.assign(merged, update);
+    });
+    console.log(merged);
+
+    const updateResult = await collection.updateOne(
+        {id: id},
+        {$set: merged}
+    );
+
+    if (updateResult.matchedCount == 0) {
+        res.send("A bathroom with that ID does not exist").status(400);
+        return;
+    }
+    res.status(200).send({"id": id, "updated": merged});
+
+
+}); 
+
+
+app.delete("/delete-bathroom", async(req, res) => {
+    const collection = await db.collection("Bathrooms");
+    const body = req.body;
+
+    const id = body["id"];
+
+    if (!id) {
+        res.send("Missing bathroom ID").status(400);
+        return;
+    }
+
+    const result = await collection.deleteOne({"id": id});
+
+    if (result.deletedCount == 0) {
+        res.send("A bathroom with that ID does not exist").status(400);
+        return;
+    }
+    res.status(200).send({"id": id, "deleted": true});
+
+})
 
 start().catch(console.dir);
