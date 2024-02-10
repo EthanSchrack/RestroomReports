43c08a3e8ad18062e532fcd9bfafef479f66d4cc
whitnotmax <91440193+whitnotmax@users.noreply.github.com>
2024-02-09 21:43:57 -0500

    bathrooms

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Hdr from './Hdr.js'
+import Bathroom from "./Bathroom.js";
+
+const bathrooms = [
+  new Bathroom("Tate Bathroom"),
+  new Bathroom("Joe Frank Bathroom"),
+  new Bathroom("Snelling bathroom")
+]
+
+let bathroomElements = bathrooms.map(function(bathroom) {
+  return <li>{bathroom.name}</li>;
+});
 
 function App() {
   return (
     <div>
       <Hdr />
+      {bathroomElements}
     </div>
   );
 }
---
+++ src/Bathroom.js
@@ -0,0 +1,5 @@
+export default class Bathroom {
+    constructor(name) {
+        this.name = name;
+    }
+}
