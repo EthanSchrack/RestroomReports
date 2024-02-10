9b1975f1c0c00ba66d1be62fda17361742b501de
whitnotmax <91440193+whitnotmax@users.noreply.github.com>
2024-02-09 22:08:23 -0500

    create toilet list

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Hdr from './Hdr.js'
 import Bathroom from "./Bathroom.js";
 
+const bathroomPictureLink = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/VillaMitre50.jpg/1280px-VillaMitre50.jpg";
+
 const bathrooms = [
-  new Bathroom("Tate Bathroom"),
-  new Bathroom("Joe Frank Bathroom"),
-  new Bathroom("Snelling bathroom")
+  new Bathroom("Tate Bathroom", "abcd", 5.0, bathroomPictureLink),
+  new Bathroom("Joe Frank Bathroom", "abcd", 5.0, bathroomPictureLink),
+  new Bathroom("Snelling bathroom", "abcd", 5.0, bathroomPictureLink)
 ]
 
 let bathroomElements = bathrooms.map(function(bathroom) {
-  return <li>{bathroom.name}</li>;
+  return (
+    <div class="bathroom-element">
+      <span>
+        <img src={bathroom.image} width="75px" height="75px"/>
+        {bathroom.name}
+        ({bathroom.rating}/5)
+      </span>
+    </div>
+  )
 });
 
 function App() {
--- src/Bathroom.js
+++ src/Bathroom.js
@@ -1,5 +1,8 @@
 export default class Bathroom {
-    constructor(name) {
+    constructor(name, description, rating, image) {
         this.name = name;
+        this.description = description;
+        this.rating = rating;
+        this.image = image;
     }
 }
