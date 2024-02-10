3624239da588749cce65f5384c95babcdfde48eb
Ivan Cao <icao3014@gmail.com>
2024-02-10 01:18:01 -0500

    Sliding Header animation

--- src/Hdr.js
+++ src/Hdr.js
@@ -5,9 +5,8 @@ import './Hdr.css'
 
 const Hdr = () => {
     return (    
-        <div className="hdr">
-            <h1>Restroom Reports</h1>
-            
+        <div className="hdr" >
+            <h1 className = "hdrwords">Restroom Reports</h1>
         </div>
     );
 }
